7ce247f70c373d7a374ab77126f51fdf3a8f8063
Alejandro <aletomasperren@gmail.com>
2025-01-21 15:40:32 -0300

    Dependencies Errors

--- client/src/types/index.ts
+++ client/src/types/index.ts
@@ -1,4 +1,4 @@
-import { object, string, number, boolean, Output, array } from 'valibot'
+import { object, string, number, boolean, Output, array} from "valibot"
 
 export const DraftProductSchema = object({
     name: string(),
